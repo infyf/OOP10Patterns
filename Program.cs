a17dcbb3c0f30bf05e0daec42512fbdd83792ebf
infyf <138444902+infyf@users.noreply.github.com>
2026-03-24 07:45:32 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+
+using OOP;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        IGameSettings settings1 = GameSettingsManager.GetInstance();
+        IGameSettings settings2 = GameSettingsManager.GetInstance();
+
+        settings1.SetVolume(80);
+        settings1.SetDifficulty("Hard");
+        settings1.ToggleSound();
+
+        Console.WriteLine("Volume: " + settings2.GetVolume());
+        Console.WriteLine("Difficulty: " + settings2.GetDifficulty());
+        Console.WriteLine("Sound Enabled: " + settings2.IsSoundEnabled());
+
+        Console.WriteLine("Same instance? " + (settings1 == settings2));
+    }
+}

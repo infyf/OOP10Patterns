e744853a2477fb29e40a3475c87d0a6ee50fa7cf
infyf <138444902+infyf@users.noreply.github.com>
2026-03-24 07:52:38 +0200

    Create IGameSettings.cs

---
+++ Creational Patterns/IGameSettings.cs
@@ -0,0 +1,14 @@
+namespace OOP
+{
+    public interface IGameSettings
+    {
+        int GetVolume();
+        void SetVolume(int value);
+
+        string GetDifficulty();
+        void SetDifficulty(string difficulty);
+
+        bool IsSoundEnabled();
+        void ToggleSound();
+    }
+}

6d267734efc4c67ceeb0afb3546db8061948ce93
infyf <138444902+infyf@users.noreply.github.com>
2026-03-28 09:05:08 +0200

    Create GamingComputerBuilder.cs

---
+++ Builder/GamingComputerBuilder.cs
@@ -0,0 +1,32 @@
+namespace OOP
+{
+    public class GamingComputerBuilder : IComputerBuilder
+    {
+        private Computer _computer = new Computer();
+
+        public void SetCPU()
+        {
+            _computer.CPU = "Intel Core i9";
+        }
+
+        public void SetGPU()
+        {
+            _computer.GPU = "NVIDIA RTX 4080";
+        }
+
+        public void SetRAM()
+        {
+            _computer.RAM = 32;
+        }
+
+        public void SetStorage()
+        {
+            _computer.Storage = 2000;
+        }
+
+        public Computer GetComputer()
+        {
+            return _computer;
+        }
+    }
+}
